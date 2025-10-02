3cac17630a0052c99621288fb5764c548e429e13
Polina Borisenko <p.danilina2000@yandex.ru>
2025-10-02 12:30:25 +0300

    add full version  project mailservice

--- src/edu/polina/mailservice/Sendable.java
+++ src/edu/polina/mailservice/Sendable.java
@@ -1,4 +1,25 @@
 package edu.polina.mailservice;
 
-public class Sendable {
+public abstract class Sendable<T> {
+    String from;
+    String to;
+    T content;
+
+    public Sendable(String from, String to, T content) {
+        this.from = from;
+        this.to = to;
+        this.content = content;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public T getContent() {
+        return content;
+    }
 }
