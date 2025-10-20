12d3fb441fb61bc064b4712d5bdd3c20c4e03105
Polina Borisenko <p.danilina2000@yandex.ru>
2025-10-20 23:56:16 +0300

    tik-tak 2.0

--- src/edu/polina/tiktak/TikTak.java
+++ src/edu/polina/tiktak/TikTak.java
@@ -3,9 +3,10 @@ package edu.polina.tiktak;
 
 public class TikTak {
     public static void main(String[] args) {
-        ThreadRunnable tr = new ThreadRunnable();
-        Thread thread1 = new Thread(tr);
-        Thread thread2 = new Thread(tr);
+        ThreadRunnable trTik = new ThreadRunnable("tik");
+        ThreadRunnable trTak = new ThreadRunnable("tak");
+        Thread thread1 = new Thread(trTik);
+        Thread thread2 = new Thread(trTak);
         thread1.start();
         thread2.start();
     }
@@ -13,32 +14,41 @@ public class TikTak {
 }
 
 class ThreadRunnable implements Runnable {
-    Object lock = new Object();
-    int i = 0;
+    static Object lock = new Object();
+    String word;
+    static int i = 0;
+
+    public ThreadRunnable(String word) {
+        this.word = word;
+    }
+
     @Override
     public void run() {
         synchronized (lock) {
             while (true) {
                 if (i % 2 == 0) {
-                    System.out.println("тик " + Thread.currentThread().getName());
+                    System.out.println(word);
                     i++;
                     lock.notify();
-                    try {
-                        lock.wait();
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
+                    while (i % 2 != 0) {
+                        try {
+                            lock.wait();
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                        }
                     }
                 }
                 if (i % 2 == 1) {
-                    System.out.println("так " + Thread.currentThread().getName());
+                    System.out.println(word);
                     i++;
                     lock.notify();
-                    try {
-                        lock.wait();
-                    } catch (InterruptedException _) {
-                        Thread.currentThread().interrupt();
+                    while (i % 2 != 1) {
+                        try {
+                            lock.wait();
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                        }
                     }
-
                 }
             }
         }
