482842bf9ea1440960510d1589ed02b200b0a409
Polina Borisenko <p.danilina2000@yandex.ru>
2025-11-12 16:55:51 +0300

    ServerDB: signup/signin + H2 persistence; remove junk; ready for review

---
+++ ServerDB/src/main/java/edu/server/SignInServlet.java
@@ -0,0 +1,43 @@
+package edu.server;
+
+import com.google.gson.Gson;
+import edu.database.DBException;
+import edu.database.DBService;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class SignInServlet extends HttpServlet {
+    private final AccountService accountService;
+
+    public SignInServlet(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    //sign in
+    public void doPost(HttpServletRequest request,
+                       HttpServletResponse response) throws ServletException, IOException  {
+        String login = request.getParameter("login");
+        String pass = request.getParameter("password");
+        var db = DBService.getInstance();
+        var user= db.findByLogin(login);
+        UserProfile profile = accountService.getUserByLogin(login);
+
+        if (profile == null && user == null) {
+            response.setContentType("text/html;charset=utf-8");
+            response.getWriter().println("Unauthorized");
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            return;
+        }
+
+        response.setContentType("text/html;charset=utf-8");
+        response.getWriter().println("Authorized: " + login);
+        response.setStatus(HttpServletResponse.SC_OK);
+    }
+
+
+}
+
